e64d17a475e2530311b5d67003d6b96e105f84a0
Carlos Zamorano <carloszr27@gmail.com>
2023-11-09 02:00:17 +0100

    service test try remove errors

--- src/app/services/alert.service.spec.ts
+++ src/app/services/alert.service.spec.ts
@@ -1,16 +1,89 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 import { AlertService } from './alert.service';
+import { Observable } from 'rxjs';
+import { Alert, AlertType } from '../models/alert.model';
 
 describe('AlertService', () => {
   let service: AlertService;
+  let snackBar: MatSnackBar;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [MatSnackBarModule],
+    });
     service = TestBed.inject(AlertService);
+    snackBar = TestBed.inject(MatSnackBar);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should display success alert', fakeAsync(() => {
+    spyOn(snackBar, 'open');
+    service.success('Success Message');
+    tick(); // wait for snackBar.open to be called
+
+    expect(snackBar.open).toHaveBeenCalledWith('Success Message', 'Cerrar', {
+      panelClass: 'success-snackbar',
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }));
+
+  it('should display error alert', fakeAsync(() => {
+    spyOn(snackBar, 'open');
+    service.error('Error Message');
+    tick(); // wait for snackBar.open to be called
+
+    expect(snackBar.open).toHaveBeenCalledWith('Error Message', 'Cerrar', {
+      panelClass: 'error-snackbar',
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }));
+
+  it('should display info alert', fakeAsync(() => {
+    spyOn(snackBar, 'open');
+    service.info('Success Message');
+    tick(); // wait for snackBar.open to be called
+
+    expect(snackBar.open).toHaveBeenCalledWith('Success Message', 'Cerrar', {
+      panelClass: 'info-snackbar',
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }));
+
+  it('should display warn alert', fakeAsync(() => {
+    spyOn(snackBar, 'open');
+    service.warn('Error Message');
+    tick(); // wait for snackBar.open to be called
+
+    expect(snackBar.open).toHaveBeenCalledWith('Error Message', 'Cerrar', {
+      panelClass: 'warn-snackbar',
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }));
+
+  it('should clear alert', () => {
+    spyOn(snackBar, 'dismiss');
+    service.clear();
+    expect(snackBar.dismiss).toHaveBeenCalled();
+  });
+
+  it('should listen to alerts', () => {
+    const alert: Alert = new Alert({
+      type: AlertType.Success,
+      message: 'Test Message',
+    });
+    const alertObserver: Observable<Alert> = service.onAlert();
+    alertObserver.subscribe((receivedAlert: Alert) => {
+      expect(receivedAlert).toEqual(alert);
+    });
+    service.alert(alert);
   });
 });
--- src/app/services/github.service.spec.ts
+++ src/app/services/github.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GithubService } from './github.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpConfigInterceptor } from '../interceptors/http-config.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 describe('GithubService', () => {
   let service: GithubService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: HttpConfigInterceptor,
+          multi: true,
+        },
+      ],
+    });
     service = TestBed.inject(GithubService);
   });
 
