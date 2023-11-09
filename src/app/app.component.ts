1955a31f1bdabd260f1352e87fb46f319bfa48a3
Carlos Zamorano <carloszr27@gmail.com>
2023-11-09 02:01:56 +0100

    add animation to router outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { ThemeService } from './services/theme.service';
+import { RouterOutlet } from '@angular/router';
+import { fadeInAnimation } from './animations/slide.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeInAnimation]
 })
 export class AppComponent {
   title = 'Odilo';
-  isDark:boolean = false;
+  isDark: boolean = false;
 
   constructor(private themeService: ThemeService) {
     this.themeService.getTheme().subscribe(theme => {
       this.isDark = theme;
     });
+  }
 
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
   }
 }
