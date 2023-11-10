cab835b5210a01a12e624cb6dc307482f6490cb0
Carlos Zamorano <carlos.zamorano@ciberexperis.es>
2023-11-10 16:52:16 +0100

    add coverage in package

--- src/app/components/search-form/search-form.component.spec.ts
+++ src/app/components/search-form/search-form.component.spec.ts
@@ -4,13 +4,15 @@ import { SearchFormComponent } from './search-form.component';
 import { AppModule } from 'src/app/app.module';
 import { ComponentsModule } from 'src/app/modules/components/components.module';
 import { UserDataService } from 'src/app/services/user-data.service';
+import { FormGroup } from '@angular/forms';
 
 describe('SearchFormComponent', () => {
   let component: SearchFormComponent;
   let fixture: ComponentFixture<SearchFormComponent>;
-  const userDataService = jasmine.createSpyObj('UserDataService', ['setUsername','getUserName']);
-
+  let userDataServiceSpy: jasmine.SpyObj<UserDataService>;
+  
   beforeEach(() => {
+    const userDataService = jasmine.createSpyObj('UserDataService', ['setUsername','getUserName']);
     TestBed.configureTestingModule({
       imports: [AppModule, ComponentsModule],
       declarations: [SearchFormComponent],
@@ -26,4 +28,11 @@ describe('SearchFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call setUsername method', () => {
+    component.myForm.controls['username'].setValue('carlos');
+    component.onSubmit();
+    expect(userDataServiceSpy.setUsername).toHaveBeenCalledWith('carlos');
+  });
+  
 });
